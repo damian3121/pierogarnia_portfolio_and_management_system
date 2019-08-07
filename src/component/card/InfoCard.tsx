7e550279977d80a1597b62512c0cc96b66c2fc35
damian3121 <damian.mscisz@gmail.com>
2019-08-07 17:48:03 +0200

    Change image url and name of InfoCard component.

--- src/component/card/InfoCard.tsx
+++ src/component/card/InfoCard.tsx
@@ -39,7 +39,7 @@ const useStyles = makeStyles((theme: Theme) =>
     }
   }));
 
-export default function RecipeReviewCard(props: Props) {
+export default function InfoCard(props: Props) {
   const classes = useStyles();
 
   return (
--- src/content/main/AboutUs.tsx
+++ src/content/main/AboutUs.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import RecipeReviewCard from '../../component/card/InfoCard';
+import InfoCard from '../../component/card/InfoCard';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,7 +20,7 @@ export function AboutUs() {
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
-          <RecipeReviewCard
+          <InfoCard
             title="Jakość"
             content="Produkcja wyrobów garmażeryjnych powstała w 2018r. 
             Tworzymy nowoczesny zakład pasjonujący się swojskimi wyrobami. 
@@ -28,17 +28,17 @@ export function AboutUs() {
             Zakupione zostały maszyny spełniające wszelkie wymogi sanitarne 
             oraz opierające się na najnowocześniejszych technologiach."
             imageHeight={250}
-            imageTrack="../../static/image/jakosc.jpg"
+            imageTrack="https://imageshack.com/i/pnQnNdLOj"
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <RecipeReviewCard
+          <InfoCard
             title="Gwarancja smaku"
             content="Gotowanie to nasza pasja! Wyroby przygotowywane są ze świeżych składników o możliwie najwyższej jakości. 
             Korzystamy ze sprawdzonych receptur przekazywanych z pokolenia na pokolenie oraz dodając nutkę własnej twórczości 
             coby nasz produkt smakował jak najlepiej."
             imageHeight={250}
-            imageTrack="../../../static/image/gwarancja_smaku.jpg"
+            imageTrack="https://imageshack.com/i/pma6PGSCj"
           />
         </Grid>
       </Grid>
