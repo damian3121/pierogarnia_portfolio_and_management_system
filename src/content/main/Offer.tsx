642a80524f8b1d157002783a07a3d978cfe0c9bb
damian3121 <damian.mscisz@gmail.com>
2019-09-10 23:16:52 +0200

    Small image and text fix.

--- src/content/main/Offer.tsx
+++ src/content/main/Offer.tsx
@@ -36,11 +36,10 @@ export function Offer() {
                 <ListParagraph content="próżniowe pakowanie (nowość!)" />
                 <ListParagraph content="zamówienia realizowane dla sklepów" />
                 <ListParagraph content="produkty przygotowane do przewozów długich i ekstremalnych np. przewoźnicy międzynarodowi" />
-                <li>elo</li>
               </Fragment>
             }
             imageHeight={250}
-            imageTrack="https://imageshack.com/i/plXktLPoj"
+            imageTrack="https://lh3.googleusercontent.com/hyYIwx_y8h_r-osMXZpdg_kZCE6JnsIdkIIIZNC0JFo30O9e7OKuBpl5Ne6FN6nYkUPMqJ9DZDYYqomYFOJmhpKg-NP3NP0sMCH6cm64_aMq2B5uNcsgYW5CvrNX-rXfj0DrJjXKPi8tDx-yWOArMNgx-mh8vIvKtic2zKSSIBDWsrre5O7D8glR-XR4GeajXQcA7k3XtJo5EKCzobbw_VgL0i1MVkYlLEtG5cJrnZg3Iih-mNB9k2hyCkPA-BhtuEUgK8m6z7ARWiudcToPHvjEm1YeRrypQJ823rxS-1MrFgd0HXvUUYB0ytqaUA2U0eUXzK_Zxoj9Ydl3rxMHwKpubBtracpzKKUeQAqnf5cnqUltdakNXGDyej4mewvKRjJj3FBGLcFO_2ePFsE4Ra3GC3T3j7gRFtP4VR2Cbxc26drrn6Ibqkr7zhlzVMwfrsvYU5QptOyezm_yFh2E4dOa90jLYblDpmRGCo0Z4JPJBdWMhTioxKeQVIh1r0P9rNK0e_21Sf1VoEAHmjVyDZhe8BW5nX1SjeABENUEgNd3PZmdW5DasZtWAXrBrl2tAPl8OXSlE5knWyXYLIxeHtYKhwU8XNBRE1-0u_pngky9MsdYDZ7FHewZ8kzIUytbFSy1IisqLqkeOBd_ngZ3p4tZrW0FASlZfY8IZC-53HryJuRYgUThykw=w1483-h585-no"
           />
         </Grid>
       </Grid>
